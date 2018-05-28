85a1fe6354bde74220d62b0880de5635fae540bd
Arshad Moolla <arshad.moolla@gmail.com>
2018-05-28 16:32:42 +0200

    update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-	   
-    // JQUERY NAV TOGGLE
+
+    // jQuery nav toggle
     $('.burger').bind('click',function(event){
         $('nav').slideToggle();
     });
 
-    $(window).resize(function(){  
-          
-        if($(window).width() > 805) {  
+    $(window).resize(function(){
+
+        if($(window).width() > 805) {
             $('nav').removeAttr('style');  
-        } 
-         
+        }
+
     });
-      
+
   });
